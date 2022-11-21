aafe85d13e49eec4f1946ef0e1611ec4cb58580f
Amar Pulavarthi <20pa1a05d4@vishnu.edu.in>
2022-11-21 22:58:16 +0530

    final changes

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-const hello2 = '"Hello Node.js!"'
+const http = require('http')
+const fs = require('fs')
 
-const hello = () => {
-  console.log(hello2)
-}
+let homeContent = ''
+let projectContent = ''
+let regsContent = ''
 
-hello()
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    throw err
+  }
+  homeContent = home
+})
+
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    throw err
+  }
+  projectContent = project
+})
+fs.readFile('mywebsite2.html', (err, registration) => {
+  if (err) {
+    throw err
+  }
+  regsContent = registration
+})
+const args = require('minimist')(process.argv.slice(1))
+
+http.createServer((request, response) => {
+  const url = request.url
+  // const portnumber = request.params.portno
+  response.writeHeader(200, { 'Content-Type': 'text/html' })
+  switch (url) {
+    case '/project':
+      response.write(projectContent)
+      response.end()
+      break
+    case '/registration':
+      response.write(regsContent)
+      response.end()
+      break
+    default:
+      response.write(homeContent)
+      response.end()
+      break
+  }
+}).listen(args.port)
